037684b3e7bc9b6e86022ac333f989b44c65adbb
Dkuran <aduran@devsu.com>
2024-10-25 09:51:36 -0500

    Added library.

---
+++ lib/index.d.ts
@@ -0,0 +1,9 @@
+export type { ChartProps } from './types.js';
+export * from './chart.js';
+export * from './typedCharts.js';
+export {
+  getDatasetAtEvent,
+  getElementAtEvent,
+  getElementsAtEvent,
+} from './utils.js';
+//# sourceMappingURL=index.d.ts.map
